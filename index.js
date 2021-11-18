2b022a8850d2dabb23d5042a4dd8bb042627f933
Andrew Wei <andrewwei913@gmail.com>
2021-11-18 09:32:21 -0800

    Structure

---
+++ index.js
@@ -0,0 +1,8 @@
+//1. API Call 
+//2. Submit Form
+//   a. Review Requirement
+//   b. Number of Reviews
+//   c. Current Location
+//   d. Radius
+//3. Result JSON to store API Call 
+//4. Populate Restaurant Cards with Data 
